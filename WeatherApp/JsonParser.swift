625eb1cfff4050c94654348cd885c0e45bc404f9
dawidplesniarski <dawidplesniarski@gmail.com>
2019-11-23 15:00:00 +0100

    jsonParser class added. Parsing nested objects

---
+++ WeatherApp/JsonParser.swift
@@ -0,0 +1,76 @@
+//
+//  JsonParser.swift
+//  WeatherApp
+//
+//  Created by Dawid on 23/11/2019.
+//  Copyright © 2019 Dawid. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+
+struct Weather: Codable{
+    let cod:Int
+    let name:String
+    let main:Main
+}
+
+struct Main: Codable{
+    let temp:Double
+    let pressure:Int
+    let humidity:Int
+}
+
+
+class JsonParser: UIViewController {
+
+    
+    
+    func loadData(){
+        let jsonUrlString = "https://api.openweathermap.org/data/2.5/weather?q=Tarnow,pl&APPID=749561a315b14523a8f5f1ef95e45864&units=metric"
+        guard let url = URL(string: jsonUrlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else { return }
+            //let dataAsString = String(data: data, encoding: .utf8)
+            //print(dataAsString ?? "")
+            do{
+                //let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                //let weather = Weather(json: json as! [String : Any])
+                let weather = try JSONDecoder().decode(Weather.self, from: data)
+                print(weather.main.temp)
+               // print(json)
+            }catch let jsonErr{
+                print(jsonErr)
+            }
+            
+        }.resume()
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}/**let jsonUrlString = "https://api.openweathermap.org/data/2.5/weather?q=Tarnow,pl&APPID=749561a315b14523a8f5f1ef95e45864&units=metric"
+guard let url = URL(string: jsonUrlString) else { return }
+
+URLSession.shared.dataTask(with: url) { (data, response, err) in
+    guard let data = data else { return }
+    //let dataAsString = String(data: data, encoding: .utf8)
+    //print(dataAsString ?? "")
+    do{
+        //let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+        //let weather = Weather(json: json as! [String : Any])
+        let weatherJSON = try JSONDecoder().decode(Weather.self, from: data)
+        print(weatherJSON.name)
+        
+       // print(json)
+    }catch let jsonErr{
+        print(jsonErr)
+    }
+    
+}.resume()*/
--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -7,26 +7,17 @@
 //
 
 import UIKit
+import Foundation
 
-struct Weather{
-    let temp:Double
-    let pressure:Int
-    let humidity:Int
-    
-}
 class WeatherViewController: UIViewController {
 
+    let jsonParser = JsonParser()
+    
+    var temperature:Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let jsonUrlString = "https://api.openweathermap.org/data/2.5/weather?q=Tarnow,pl&APPID=749561a315b14523a8f5f1ef95e45864&units=metric"
-        guard let url = URL(string: jsonUrlString) else { return }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            guard let data = data else { return }
-            let dataAsString = String(data: data, encoding: .utf8)
-            print(dataAsString!)
-        }.resume()
+        jsonParser.loadData()
     }
     
 }
