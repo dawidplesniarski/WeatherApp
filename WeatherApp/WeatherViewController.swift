241b527f24989be66da45d6cf478a963aa5e073d
dawidplesniarski <dawidplesniarski@gmail.com>
2019-11-26 13:12:49 +0100

    the background of the application changes depending on the weather conditions

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -16,6 +16,7 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var weatherBackgroundImageView: UIImageView!
     
     
     var userCityName: String = "Warszawa"
@@ -39,6 +40,19 @@ class WeatherViewController: UIViewController {
             self.descriptionLabel.text = self.jsonParser.weatherDescription
             self.cityNameLabel.text = self.userCityName
             self.weatherIconImageView.image = UIImage(named: self.jsonParser.weatherIcon)
+                if(self.jsonParser.weatherDescription.contains("mist")){
+                    self.weatherBackgroundImageView.image = UIImage(named: "mist_background")
+                }else if(self.jsonParser.weatherDescription.contains("rain")){
+                    self.weatherBackgroundImageView.image = UIImage(named: "rain_background")
+                }else if(self.jsonParser.weatherDescription.contains("clear")){
+                    self.weatherBackgroundImageView.image = UIImage(named: "clear_sky_background")
+                }else if(self.jsonParser.weatherDescription.contains("clouds")){
+                    self.weatherBackgroundImageView.image = UIImage(named:"clouds_background")
+                }else if(self.jsonParser.weatherDescription.contains("thunder")){
+                    self.weatherBackgroundImageView.image = UIImage(named:"thunder_background")
+                }else if(self.jsonParser.weatherDescription.contains("snow")){
+                    self.weatherBackgroundImageView.image = UIImage(named: "snow_background")
+                }
             }else{
                 self.dismiss(animated: true, completion: nil)
             }
